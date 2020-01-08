1350e4610facc2f3a3411747e38ed8e26d59d0ce
heitor nobuo matsusaki nozu <heitor.nozu@cbyk.com.br>
2020-01-08 10:42:34 -0300

    primeiro commite

---
+++ src/main/java/br/com/nobu/nobuService/NobuServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.nobu.nobuService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nobu/nobuService/NobuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nobu.nobuService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
