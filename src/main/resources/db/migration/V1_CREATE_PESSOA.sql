ff80a661e7dcfc68e749d3f9479cd7bb6cea2b81
heitor nobuo matsusaki nozu <heitor.nozu@cbyk.com.br>
2020-01-14 11:15:59 -0300

    testando deploy

---
+++ src/main/resources/db/migration/V1_CREATE_PESSOA.sql
@@ -0,0 +1,8 @@
+	INSERT INTO films (nome, email, dataNascimento, cargo)
+    VALUES ('TESTE NOBU 1', 'teste@gmail.com', '1998-03-03',  'Drama');
+    INSERT INTO films (nome, email, dataNascimento, cargo)
+    VALUES ('TESTE NOBU 2', 'teste@gmail.com', '1998-03-03',  'Drama');
+    INSERT INTO films (nome, email, dataNascimento, cargo)
+    VALUES ('TESTE NOBU 3', 'teste@gmail.com', '1998-03-03',  'Drama');
+    INSERT INTO films (nome, email, dataNascimento, cargo)
+    VALUES ('TESTE NOBU 4', 'teste@gmail.com', '1998-03-03',  'Drama');
